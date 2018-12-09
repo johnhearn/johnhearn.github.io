3c47d9a55982b6f2745197729081143603d78856
John Hearn <hearn.john@gmail.com>
2018-12-09 22:37:39 +0100

    Site updated at 2018-12-09 21:37:39 UTC

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'jekyll'
-gem 'rouge'
-gem 'jekyll-time-to-read'
-gem 'jekyll-graphviz'
--- serve.sh
+++ serve.sh
@@ -1,1 +1,1 @@
-bundle exec jekyll serve --livereload --incremental --drafts --future
+bundle exec jekyll serve --watch --force_polling --livereload --incremental --drafts --future
